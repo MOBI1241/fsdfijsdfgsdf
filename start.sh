196ed0ea4ab0f6c736db99b6dca11779a54c148f
MOBI1241 <158516807+MOBI1241@users.noreply.github.com>
2024-04-12 00:04:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MOBI1241/fsdfijsdfgsdf.git /AutoFilter
+  git clone https://github.com/MOBI1241/fsdfijsdfgsdf.git /fsdfijsdfgsdf
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilter
+  git clone $UPSTREAM_REPO /fsdfijsdfgsdf
 fi
-cd /AutoFilter
+cd /fsdfijsdfgsdf
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ ʟᴜᴄʏ ʙᴏᴛ...."
 python3 bot.py
